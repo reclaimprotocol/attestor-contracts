288623d12515c0b16d8c172b58b73393b120e6ea
hadi-saleh14 <hadi@creatoros.co>
2025-03-04 14:28:27 +0300

    add mechain config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+const { PRIVATE_KEY } = process.env
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.28'
+  networks: {
+    hardhat: {},
+    'mechain-testnet': {
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      url: 'https://testnet-rpc.mechain.tech'
+    }
+  },
+  solidity: {
+    version: '0.8.28',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 }
 
 export default config
